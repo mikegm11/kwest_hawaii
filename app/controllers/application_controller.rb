623d98e6fc79789f29072aeb6e5604556a4644a2
Mike <mikegmiller11@gmail.com>
2019-07-15 17:59:12 +0000

    started putting pages together

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+    def home
+        render("pages/home.html.erb")
+    end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  #LANDING PAGE route
+  
+  match("/", { :controller => "application", :action => "home", :via => "get" })
+  
+  
 end
