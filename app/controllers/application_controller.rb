6b804ccaee70ece2b638a7d39a63c610e1f45ee2
Mike <mikegmiller11@gmail.com>
2019-07-17 00:17:59 +0000

    added page functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
     def home
-        render("pages/home.html.erb")
+      a = Time.zone.today.to_s
+      if a == Date.parse("July 15 2020").to_s
+        render("pages/home1.html.erb")
+      elsif a == Date.parse("July 16 2019").to_s
+        render("pages/home2.html.erb")
+      else
+        render("pages/homeerror.html.erb")
+      end
     end
     
     def itinerary
